d8d13835ab237e819f1b52ada3b98679a59a5c87
Maarten Demandt <maartendemandt@gmail.com>
2020-01-31 22:57:30 +0100

    info!

--- resources/views/Public/ViewEvent/Partials/EventOrganiserSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventOrganiserSection.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-12">
                 <div class="event_organiser_details" property="organizer" typeof="Organization">
                     <p style="margin-bottom: 15px">Vragen of opmerkingen? Neem gerust contact met ons op!</p>
-                    <a href="mailto: pr@neetteredde.nl"><span class="glyphicon glyphicon-envelope" style="top: 3px; margin-right: 8px" aria-hidden="true"></span>  pr@neetteredde.nl</a>
+                    <a href="mailto: info@neetteredde.nl"><span class="glyphicon glyphicon-envelope" style="top: 3px; margin-right: 8px" aria-hidden="true"></span>  pr@neetteredde.nl</a>
                     <div style="margin-top: 20px; width: 100%; display:flex; align-items: center; flex: 1 1 0; flex-direction: row; flex-wrap: nowrap;">
                         <div style="margin: 20px">
                             <img style="max-width:100%; flex: 1 1 0" alt="tec-audio" src="{{asset('assets/images/tec_audio.png')}}" property="logo">
