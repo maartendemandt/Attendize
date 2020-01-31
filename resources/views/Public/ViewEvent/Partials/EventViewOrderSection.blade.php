323b193e4eada0fda1922aecc01a164ca822432a
Maarten Demandt <maartendemandt@gmail.com>
2020-01-31 23:10:34 +0100

    hope

--- resources/views/Public/ViewEvent/Partials/EventViewOrderSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventViewOrderSection.blade.php
@@ -83,11 +83,6 @@
                         <div class="offline_payment_instructions well">
                             <p>
                                 Graag het totaalbedrag van {{$order->event->currency_symbol}}{{number_format($order->total_amount, 2)}} binnen 7 werkdagen overmaken naar de rekening:<br><br>
-                                
-                                Betaal met onderstaande link:<br>
-                                <a class="btn btn-lg btn-success card-submit"  style="margin-top: 15px" target="_BLANK" href="https://betaalverzoek.rabobank.nl/betaalverzoek/?id=P4cTLxgLRJmBpVmS933PEw">Betaal hier!</a><br><br>
-                
-                                of <br><br>
                                                
                                 NL76 RABO 0315985305 t.a.v. Stichting Neet te Redde <br>
                                 Onder vermelding van je voor- en achternaam. <br> <br>
